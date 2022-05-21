8d3704a99558f105615632fcbab699e98fc5b063
stanman2020 <stanelo2000@yahoo.com>
2022-05-21 16:15:22 +0000

    remote commit

--- collins.sh
+++ collins.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/bashh
 echo "please enter username"
 read username
 echo "please enter password"
 read pwd1
 echo "please re-enter password"
 read pwd2
+
 if
 [ $pwd1 != $pwd2 ];
 then
@@ -14,6 +15,15 @@ elif
 then
 sudo useradd $username
 echo $pwd1 | sudo passwd $username --stdin
+if 
+[ $pwd1 != $pwd2 ];
+then 
+echo  "incorrect password"
+elif
+[ $pwd1 == $pwd2 ] && [[ $pwd1 =~ [A-Z] ]] && [[ $pwd1 =~ [[:punct:]] ]] && [ ${#pwd1} -ge 8 ] && [[ $pwd1 =~ [[:digit:]] ]];
+then 
+sudo useradd $username
+echo $pwd1 | sudo passwd $username --stdin 
 echo "congratulation $username, your account have been created"
 else
 echo "account creation not successful"
---
+++ test1.sh
@@ -0,0 +1,7 @@
+echo "please enter your price"
+read p
+if [ $p == 2000 ]
+then 
+echo "the price is good for the business"
+echo "please deliver the product"
+fi
